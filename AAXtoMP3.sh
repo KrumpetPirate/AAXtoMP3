ba42d36f80f66d6fdb2307441a88f60a1b6b5d8a
Victor Engmark <victor.engmark@gmail.com>
2017-01-04 23:36:44 +0000

    Use a temporary working directory for safety

--- AAXtoMP3.sh
+++ AAXtoMP3.sh
@@ -12,18 +12,20 @@ then
     exit 1
 fi
 
-trap 'rm -f "tmp.txt"' EXIT TERM INT
+trap 'rm --recursive --force "${working_directory}"' EXIT
+working_directory="$(mktemp --directory)"
+metadata_file="${working_directory}/metadata.txt"
 
 for path
 do
     debug "Decoding ${path} with AUTHCODE ${auth_code}..."
 
-    ffmpeg -i "${path}" 2> tmp.txt
-    title=$(grep -a -m1 -h -r "title" tmp.txt | head -1 | cut -d: -f2- | xargs echo )
+    ffmpeg -i "${path}" 2> "$metadata_file"
+    title=$(grep -a -m1 -h -r "title" "$metadata_file" | head -1 | cut -d: -f2- | xargs echo )
     title=$(echo "${title}" | sed -e 's/ (Unabridged)//' | xargs echo )
-    artist=$(grep -a -m1 -h -r "artist" tmp.txt | head -1 | cut -d: -f2- | xargs echo )
-    genre=$(grep -a -m1 -h -r "genre" tmp.txt | head -1 | cut -d: -f2- | xargs echo )
-    bitrate=$(grep -a -m1 -h -r "bitrate" tmp.txt | head -1 | rev | cut -d: -f 1 | rev | egrep -o [0-9]+ | xargs echo )
+    artist=$(grep -a -m1 -h -r "artist" "$metadata_file" | head -1 | cut -d: -f2- | xargs echo )
+    genre=$(grep -a -m1 -h -r "genre" "$metadata_file" | head -1 | cut -d: -f2- | xargs echo )
+    bitrate=$(grep -a -m1 -h -r "bitrate" "$metadata_file" | head -1 | rev | cut -d: -f 1 | rev | egrep -o [0-9]+ | xargs echo )
     bitrate="${bitrate}k"
     output=$(echo "${title}" | sed -e 's/\:/-/g' | xargs echo )
     output_directory="${genre}/${artist}/${title}"
@@ -45,11 +47,9 @@ do
             mv "${output} - ${chapter}.mp3" "${output_directory}"
             set +x
         fi
-    done < tmp.txt
+    done < "$metadata_file"
     mv "${output}.mp3" "${output_directory}"
     debug "Done creating chapters. Single file and chaptered files contained in ${output_directory}."
-
-    rm tmp.txt
 
     debug "Extracting cover into ${output_directory}/cover.jpg..."
     ffmpeg -v error -activation_bytes "${auth_code}" -i "${path}" -an -vcodec copy "${output_directory}/cover.jpg"
