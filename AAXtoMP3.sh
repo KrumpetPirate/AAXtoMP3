636140f1ca6c3707f589e67d6818222036a8a108
Victor Engmark <victor.engmark@gmail.com>
2017-01-04 23:28:32 +0000

    Use lower case variable names

--- AAXtoMP3.sh
+++ AAXtoMP3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-AUTHCODE=$1
+auth_code=$1
 shift
 
 if ! command -v ffmpeg 2> /dev/null ; then
@@ -10,42 +10,42 @@ fi
 trap 'rm -f "tmp.txt" ; exit 0 ;' EXIT TERM INT
 
 while [ $# -gt 0 ]; do
-    FILE="$1"
-    echo "$(date "+%F %T%z") Decoding ${FILE} with AUTHCODE ${AUTHCODE}..."
+    path="$1"
+    echo "$(date "+%F %T%z") Decoding ${path} with AUTHCODE ${auth_code}..."
 
-    ffmpeg -i "${FILE}" 2> tmp.txt
-    TITLE=$(grep -a -m1 -h -r "title" tmp.txt | head -1 | cut -d: -f2- | xargs echo )
-    TITLE=$(echo "${TITLE}" | sed -e 's/ (Unabridged)//' | xargs echo )
-    ARTIST=$(grep -a -m1 -h -r "artist" tmp.txt | head -1 | cut -d: -f2- | xargs echo )
-    GENRE=$(grep -a -m1 -h -r "genre" tmp.txt | head -1 | cut -d: -f2- | xargs echo )
-    BITRATE=$(grep -a -m1 -h -r "bitrate" tmp.txt | head -1 | rev | cut -d: -f 1 | rev | egrep -o [0-9]+ | xargs echo )
-    BITRATE="${BITRATE}k"
-    OUTPUT=$(echo "${TITLE}" | sed -e 's/\:/-/g' | xargs echo )
-    OUTPUT_DIR="${GENRE}/${ARTIST}/${TITLE}"
+    ffmpeg -i "${path}" 2> tmp.txt
+    title=$(grep -a -m1 -h -r "title" tmp.txt | head -1 | cut -d: -f2- | xargs echo )
+    title=$(echo "${title}" | sed -e 's/ (Unabridged)//' | xargs echo )
+    artist=$(grep -a -m1 -h -r "artist" tmp.txt | head -1 | cut -d: -f2- | xargs echo )
+    genre=$(grep -a -m1 -h -r "genre" tmp.txt | head -1 | cut -d: -f2- | xargs echo )
+    bitrate=$(grep -a -m1 -h -r "bitrate" tmp.txt | head -1 | rev | cut -d: -f 1 | rev | egrep -o [0-9]+ | xargs echo )
+    bitrate="${bitrate}k"
+    output=$(echo "${title}" | sed -e 's/\:/-/g' | xargs echo )
+    output_directory="${genre}/${artist}/${title}"
 
-    ffmpeg -v error -stats -activation_bytes "${AUTHCODE}" -i "${FILE}" -vn -c:a libmp3lame -ab "${BITRATE}" "${OUTPUT}.mp3"
+    ffmpeg -v error -stats -activation_bytes "${auth_code}" -i "${path}" -vn -c:a libmp3lame -ab "${bitrate}" "${output}.mp3"
 
-    echo "$(date "+%F %T%z") Created ${OUTPUT}.mp3."
+    echo "$(date "+%F %T%z") Created ${output}.mp3."
 
-    echo "$(date "+%F %T%z") Extracting chaptered mp3 files from ${OUTPUT}.mp3..."
-    mkdir -p "${OUTPUT_DIR}"
+    echo "$(date "+%F %T%z") Extracting chaptered mp3 files from ${output}.mp3..."
+    mkdir -p "${output_directory}"
     set -x
     while read -r first _ _ start _ end; do
         if [[ "${first}" = "Chapter" ]]; then
             read -r
             read -r _ _ chapter
-            ffmpeg -v error -stats -i "${OUTPUT}.mp3" -ss "${start%?}" -to "${end}" -acodec copy "${OUTPUT} - ${chapter}.mp3" < /dev/null
-            mv "${OUTPUT} - ${chapter}.mp3" "${OUTPUT_DIR}"
+            ffmpeg -v error -stats -i "${output}.mp3" -ss "${start%?}" -to "${end}" -acodec copy "${output} - ${chapter}.mp3" < /dev/null
+            mv "${output} - ${chapter}.mp3" "${output_directory}"
             set +x
         fi
     done < tmp.txt
-    mv "${OUTPUT}.mp3" "${OUTPUT_DIR}"
-    echo "$(date "+%F %T%z") Done creating chapters. Single file and chaptered files contained in ${OUTPUT_DIR}."
+    mv "${output}.mp3" "${output_directory}"
+    echo "$(date "+%F %T%z") Done creating chapters. Single file and chaptered files contained in ${output_directory}."
 
     rm tmp.txt
 
-    echo "$(date "+%F %T%z") Extracting cover into ${OUTPUT_DIR}/cover.jpg..."
-    ffmpeg -v error -activation_bytes "${AUTHCODE}" -i "${FILE}" -an -vcodec copy "${OUTPUT_DIR}/cover.jpg"
+    echo "$(date "+%F %T%z") Extracting cover into ${output_directory}/cover.jpg..."
+    ffmpeg -v error -activation_bytes "${auth_code}" -i "${path}" -an -vcodec copy "${output_directory}/cover.jpg"
     echo "$(date "+%F %T%z") Done."
 
     shift
